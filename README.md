66d3a142ce720ec9c2ee07ed4bf13e54403d351a
nulll <nulllifeart@gmail.com>
2022-04-03 02:10:54 +0800

    1

---
+++ README.md
@@ -0,0 +1,9 @@
+---
+git submodule
+
+1. git submodule add `REPOSITORY`
+2. add `include 'REPOSITORY'` in settings.gradle
+3. add `implementation project(':REPOSITORY')` in build.gradle dependencies
+4. git submodule init
+
+---
---
+++ m1/src/main/java/org/aery/git/m1/Kerker.java
@@ -0,0 +1,11 @@
+package org.aery.git.m1;
+
+import org.misty.ut.tool.StringSplitter;
+
+public class Kerker {
+
+    public static void main(String[] args) {
+        StringSplitter stringSplitter = new StringSplitter();
+    }
+
+}
